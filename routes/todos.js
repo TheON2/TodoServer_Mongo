fb35024f25ad6b849a9e4ba74d75a98ed4c7dfb6
TheON2 <sniperad@naver.com>
2023-07-02 23:03:14 +0900

    Update : pagination logic

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,78 @@
 module.exports = function(app, Todo)
 {
     app.get('/todos', async (req, res) => {
-        const todos = await Todo.find({});
-        res.json(todos);
+        try {
+            const doneTodos = await Todo.find({done: true}).limit(4);
+            const notDoneTodos = await Todo.find({done: false}).limit(4);
+            res.json([...doneTodos, ...notDoneTodos]);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({message: "Server error"});
+        }
     });
+
+    app.post('/todos/working/infinite', async (req, res) => {
+        try {
+            const page = req.body.page;
+            console.log(page)
+            const size = 10; // 한 페이지당 로드될 아이템의 수
+            const workingTodos = await Todo.find({done: false})
+              .skip(page * size)
+              .limit(size);
+            res.json([...workingTodos]);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({message: "Server error"});
+        }
+    });
+
+    app.post('/todos/done/infinite', async (req, res) => {
+        try {
+            const page = req.body.page;
+            console.log(page)
+            const size = 10; // 한 페이지당 로드될 아이템의 수
+            const doneTodos = await Todo.find({done: true})
+              .skip(page * size)
+              .limit(size);
+            res.json([...doneTodos]);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({message: "Server error"});
+        }
+    });
+
+  app.post('/todos/working/pagination', async (req, res) => {
+    try {
+      const page = req.body.page;
+      console.log(page)
+      const size = 8; // 한 페이지당 로드될 아이템의 수
+      const workingTodos = await Todo.find({done: false})
+        .skip(page * size)
+        .limit(size);
+      const Todos = await Todo.find({done: false})
+      console.log(Todo)
+
+      res.json({ todos:workingTodos, pageNum:Math.ceil(Todos.length/8)});
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({message: "Server error"});
+    }
+  });
+
+  app.post('/todos/done/pagination', async (req, res) => {
+    try {
+      const page = req.body.page;
+      console.log(page)
+      const size = 8; // 한 페이지당 로드될 아이템의 수
+      const doneTodos = await Todo.find({done: true})
+        .skip(page * size)
+        .limit(size);
+      res.json([...doneTodos]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({message: "Server error"});
+    }
+  });
 
     app.post('/todos', async (req, res) => {
         const newTodo = new Todo(req.body);
